35f9ae7eb7f845695bc10e6f47123504ef87a9ca
pascoaland <andressa.pascoal00886178@sesisenaipr.org.br>
2025-06-11 14:51:41 +0000

    Atividade resolvida

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,9 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
-
+Nome = 'Andressa'
+Profissao = 'Engenheira Mecatrônica'
 # Declare variáveis de cada tipo numérico
-
-
+Idade = 27
 # Declare uma variável do tipo booleano
+Salario = 6300.00
